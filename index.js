c5c71a3edb2036ff615a3ab86225ec63f365125d
nthokiesther09 <nthokiesther09@gmail.com>
2022-12-02 00:04:02 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,20 @@
+const divide = () => {
+    console.log(`dividing ${2000}`);
+    console.log(`dividing ${100}`);
+    return 2000/100;}
+    divide(2000/100);
+    // =>20
+
+    
+    const add = (a, b) => (a + b);
+    add(3, 4);
+    // =>7
+
+
+    const square = () => {
+        let x = 2
+        console.log('square ${2}');
+        return x** 2;}
+    square (x**2);
+//     // =>4
+
