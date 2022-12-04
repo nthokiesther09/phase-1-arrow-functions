3d0715558ddd72aa824eeab6b26a7712a5421cb0
nthokiesther09 <nthokiesther09@gmail.com>
2022-12-04 07:55:53 +0300

    completed assignment,re-submission

--- index.js
+++ index.js
@@ -1,20 +1,8 @@
-const divide = () => {
-    console.log(`dividing ${2000}`);
-    console.log(`dividing ${100}`);
-    return 2000/100;}
-    divide(2000/100);
-    // =>20
+const divide =() =>{
+    return(2000/100)
+}
 
-    
-    const add = (a, b) => (a + b);
-    add(3, 4);
-    // =>7
+const square = (parameternum) =>(parameternum ** parameternum);
 
-
-    const square = () => {
-        let x = 2
-        console.log('square ${2}');
-        return x** 2;}
-    square (x**2);
-//     // =>4
+const add = (parameter1, parameter2) => (parameter1 + parameter2);
 
